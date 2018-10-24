71246026376a1358c373a46c9df8243c48799baf
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-10-24 10:50:15 +0200

    Finalizado añadiendo lector por teclado en el 4.3

--- boletin4/Boletin4.java
+++ boletin4/Boletin4.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Boletin4 {
 
     public static void main(String[] args) {
-//4.1
         Scanner ler = new Scanner(System.in);
+//4.1
         Coche Obx1 = new Coche();
         int acelero = ler.nextInt();
         int freno = ler.nextInt();
@@ -29,8 +29,10 @@ public class Boletin4 {
         System.out.println("El circulo tiene un area de "+ area + " y una longitud de "+ longitud);
 //4.4
         Almacen Obx4=new Almacen (48,59);
-        Obx4.engadirPatacas(100);
-        Obx4.engadirPolbo (100);
+        System.out.println("Introzuca patatas a añadir al almace.");
+        Obx4.engadirPatacas(ler.nextInt());
+        System.out.println("Introzuca polbo a añadir al almace.");
+        Obx4.engadirPolbo (ler.nextInt());
         int clientes=Obx4.podeAtender();
         System.out.println("Patacas="+ Obx4.amosarPatacas()+ "Polbo="+ Obx4.amosarPolbo());
         System.out.println("En total pode atender a: "+clientes+" clientes");
